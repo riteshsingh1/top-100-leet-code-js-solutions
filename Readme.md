76923d39d19dbc9d8b86dc774d0a20b5e762c42f
Ritesh Singh <1124ritesh@gmail.com>
2024-01-29 22:15:07 +0530

    readme added with two sum
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Top 100 Leetcode interview questions solutions in javascript and typescript
+
+## Javascript
+
+1. [Two Sum](./js/two-sum.js) - [Leet Code Link](https://leetcode.com/problems/two-sum/)
+
+
+## Typescript
+
+1. [Two Sum](./ts/two-sum.ts) - [Leet Code Link](https://leetcode.com/problems/two-sum/)
---
+++ ts/two-sum.ts
@@ -0,0 +1,52 @@
+/**
+    Problem: https://leetcode.com/problems/two-sum/
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You can return the answer in any order.
+
+    Example 1:
+
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+    Example 2:
+
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
+    Example 3:
+
+    Input: nums = [3,3], target = 6
+    Output: [0,1]
+ */
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * time complexity: O(n)
+ * space complexity: O(n)
+ * @param nums array of numbers
+ * @param target number
+ * @returns  array of two numbers that add up to target
+ * check on codepen : https://codepen.io/1124ritesh/pen/abMEaZY
+ */
+function twoSumTs(nums: number[], target: number): number[] {
+  const map = new Map<number, number>();
+  // We are using a single loop that iterates through the array of numbers once.
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    // We are using a map to store the complement and the index of the number.
+    //  map.has operation and map.set operation take constant time on average and O(n) in the worst case.
+    if (map.has(complement)) {
+      // map.get operation takes constant time.
+      return [map.get(complement)!, i];
+    }
+    // map.set operation takes constant time.
+    map.set(nums[i], i);
+  }
+  // If no solution is found, we return an empty array.
+  return [];
+}
+
+console.log(twoSumTs([2, 7, 11, 15], 17));
+
+
+  // check output - https://codepen.io/1124ritesh/pen/abMEaZY
